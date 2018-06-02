94e508b0f60f776a81c0611962c9a27a653024cd
Chandler <watson@facni.com>
2018-06-01 23:43:02 -0600

    Update waypoint_reader to Pose2D

--- src/waypoint_reader.py
+++ src/waypoint_reader.py
@@ -1,39 +1,41 @@
 #!/usr/bin/env python
 import rospy
 from sensor_msgs.msg import NavSatFix
+from geometry_msgs.msg import Pose2D
 
 
 class WaypointReader():
     WAYPOINT_FILE = '/home/nvidia/waypoints.txt'
-    THRESHOLD = 10  # meters
+    THRESHOLD = 10 # approx meters
 
     def __init__(self):
         with open(self.WAYPOINT_FILE, 'r') as f:
-            self.waypoints = [self.convert_to_fix(t) for t in 
+            self.waypoints = [self.convert_to_pose(t) for t in
                               f.read().splitlines()]
 
-        self.pub = rospy.Publisher('waypoint', NavSatFix, queue_size=10)
+        self.pub = rospy.Publisher('waypoint', Pose2D, queue_size=10)
         self.sub = rospy.Subscriber('fix', NavSatFix, self.fix_callback)
         self.waypoint = self.waypoints.pop(0)
 
     @staticmethod
-    def convert_to_fix(waypoint_text):
-        lat, lon = [float(val) for val in waypoint_text.split(',')]
-        fix = NavSatFix()
-        fix.latitude = lat
-        fix.longitude = lon
-        return fix
+    def convert_to_pose(waypoint_text):
+        lat, lon, head = [float(val) for val in waypoint_text.split(',')]
+        pose = Pose2D()
+        pose.x = lat
+        pose.y = lon
+        pose.theta = head
+        return pose
 
     def at_goal(self, fix):
-        delta_lat = fix.latitude - self.waypoint.latitude
-        delta_lon = fix.longitude - self.waypoint.longitude
+        delta_lat = 85000 * (fix.latitude - self.waypoint.x)
+        delta_lon = 85000 * (fix.longitude - self.waypoint.y)
         dist = (delta_lat**2 + delta_lon**2)**0.5
         return dist < self.THRESHOLD
 
     def fix_callback(self, fix):
         if self.at_goal(fix) and len(self.waypoints) > 0:
-            self.waypoint = convert_to_fix(self.waypoints.pop(0))
-        self.pub.publish(self.waypoint) 
+            self.waypoint = convert_to_pose(self.waypoints.pop(0))
+        self.pub.publish(self.waypoint)
 
 
 def main():
